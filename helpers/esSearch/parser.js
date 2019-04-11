03a55b1acce75153b4ab0bccf1f73deffd3a2a5d
Rémy MEJA <remymeja@free.fr>
2019-04-11 08:02:30 +0200

    draft version

---
+++ helpers/esSearch/parser.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const peg  = require('pegjs'),
+      fs   = require('fs'),
+      path = require('path')
+;
+
+const aggsGrammar = fs.readFileSync(path.join(__dirname, 'search.grammar'), 'utf8'),
+      laxeJsonGrammar = fs.readFileSync(path.join(__dirname, '../laxeJson/laxeJson.grammar'), 'utf8'),
+      parser      = peg.generate(aggsGrammar + '\n' + laxeJsonGrammar);
+
+module.exports.parse = _parse;
+
+function _parse (aggsQuery) {
+  try {
+    return parser.parse(aggsQuery);
+  } catch (err) {
+    err.isPeg = true;
+    err.label = 'Elastic search grammar';
+    throw err;
+  }
+}
